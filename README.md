af7e32a9167e5f207bd065d4f2a4118980213472
Ronal Rojas <ronalrojasroquej@gmail.com>
2023-07-31 23:50:05 -0500

    second commit

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ In the project directory, you can run:
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
-
+el build se creo
 ### `npm test`
 
 Launches the test runner in the interactive watch mode.\
--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -3,44 +3,51 @@ import '../common/style-home.css';
 import Lineas from '../components/effect.component';
 import { BsFillCaretDownFill } from "react-icons/bs";
 
-  
+
 const Home = () => {
-    const name = "Ronal Rojas Roque";
-    const Icon = [
-        {
-            icon: <BsFillCaretDownFill />
-        },
-    ]
-    
-    const [visible, setVisible] = useState(true);
-    
-      useEffect(() => {
-        const timer = setTimeout(() => {
-          setVisible(false);
-        }, 3000); // Desaparece después de 3 segundos (ajusta este valor según tus necesidades)
-    
-        return () => {
-          clearTimeout(timer);
-        };
-      }, []);
+  const name = "Ronal Rojas Roque";
+  const Icon = [
+    {
+      icon: <BsFillCaretDownFill />
+    },
+  ]
+
+  const [visible, setVisible] = useState(true);
+  const [showText, setShowText] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 5000); // Desaparece después de 
+
+    const showTextTimer = setTimeout(() => {
+      setShowText(true);
+    }, 5000);
+
+    return () => {
+      clearTimeout(timer);
+      clearTimeout(showTextTimer);
+    };
+  }, []);
 
 
-    return (
-        <div className='page-home'>
-            <div className="page-home-item" id="home">
-                <h1 className="h1-home">HOLA MI NOMBRE ES <i className='i-home'>{name}</i></h1>
-                <h2 className={`typing-effect ${visible ? 'visible' : ''}`}>Soy un Programador Full Stack </h2>
+  return (
+    <div className='page-home'>
+      <div className="page-home-item" id="home">
+        <h1 className="h1-home">HOLA MI NOMBRE ES <i className='i-home'>{name}</i></h1>
+        {showText  && (
+          <h2 className={`typing-effect ${visible ? 'visible' : ''}`}>Soy un Programador Full Stack</h2>
+          )}
 
-                <div className="icon">
-                  <ul>
-                  <a className='lin' href="#about">{Icon[0].icon}</a>
-                  </ul>
-                  </div>
-            </div>
-
-            <Lineas />
+        <div className="icon">
+          <ul>
+            <a className='lin' href="#about">{Icon[0].icon}</a>
+          </ul>
         </div>
-    );
+      </div>
+
+      <Lineas />
+    </div>
+  );
 };
 
 export default Home;
